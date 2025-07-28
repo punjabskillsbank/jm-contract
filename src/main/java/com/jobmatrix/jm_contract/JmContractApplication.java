bfc15c6c06894f69a3797b189043e8c63015f40d
Ivkaran Singh <ivkaransingh@macbook-pro.mynetworksettings.com>
2025-07-27 21:51:15 -0400

    JMK_133: initial commit

---
+++ src/main/java/com/jobmatrix/jm_contract/JmContractApplication.java
@@ -0,0 +1,13 @@
+package com.jobmatrix.jm_contract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmContractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmContractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobmatrix/jm_contract/JmContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobmatrix.jm_contract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmContractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
